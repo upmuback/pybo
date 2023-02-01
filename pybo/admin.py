0f4060d37d4882ad556251388841e374df399600
Jason Ji <upmuback@gmail.com>
2023-02-01 11:13:56 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from.models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# Register your models here.
+
+
